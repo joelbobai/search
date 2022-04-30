f09dcd4bb99c10358bbceccb2eb3259865a9cee2
joelbobai <90658305+joelbobai@users.noreply.github.com>
2022-04-30 10:15:19 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = ({ setDarkTheme, darkTheme }) => (
     <div className="flex justify-between items-center space-x-5 w-screen ">
       <Link to="/">
         <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-50 dark:text-gray-900">
-          Bobai 🔎
+          mumu 🔎
         </p>
       </Link>
       <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg">{darkTheme ? '💡 Light' : '🌙 Dark'}</button>
